8b29a5d9b7c907ad915bd6f79b9276f9a23e7023
Yousef <yousefayman2000500@gmail.com>
2023-10-15 18:05:02 +0300

    manging the user input state

--- src/Components/AddUserForm/AddUserForm.jsx
+++ src/Components/AddUserForm/AddUserForm.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 const AddUserForm = () => {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
+
+  const userNameChangeHandler = (e) => {
+    setUserName(e.target.value);
+  };
+  const userAgeChangeHandler = (e) => {
+    setUserAge(e.target.value);
+  };
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(userName, userAge);
   };
   return (
     <>
@@ -10,11 +23,21 @@ const AddUserForm = () => {
       >
         <div className="flex gap-6">
           <label htmlFor="username">Username</label>
-          <input className="bg-white" type="text" id="username" />
+          <input
+            className="bg-white"
+            type="text"
+            id="username"
+            onChange={userNameChangeHandler}
+          />
         </div>
         <div className="flex gap-6">
           <label htmlFor="age">Age(years)</label>
-          <input className="bg-white" type="number" id="age" />
+          <input
+            className="bg-white"
+            type="number"
+            id="age"
+            onChange={userAgeChangeHandler}
+          />
         </div>
         <button
           className="transition-all mt-6 text-white hover:bg-green-300 hover:text-black bg-green-600 duration-300"
